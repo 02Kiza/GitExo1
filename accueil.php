32410262504391da788a51d5bd47cfdc14b0b901
Yohann ASSEMIEN <yasse@lmdsio.com>
2024-09-11 14:17:28 +0200

    Mon Projet Version 1

---
+++ accueil.php
@@ -0,0 +1,21 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    +
+    <title>Accueil</title>
+</head>
+<?php
+if (isset($_POST["NM"])  && !empty($_POST["NM"] && isset($_POST["PR"])  && !empty($_POST["PR"])))
+{
+
+    $nom = $_POST["NM"];
+    $prenom = $_POST["PR"];
+   
+
+}
+    echo "Bonjour";
+{
+  echo "Attention, erreur de saisi";
+}
+
+?>
